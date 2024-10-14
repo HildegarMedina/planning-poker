71374b62ba2c74ce45c2f102139f044a42ca4a7a
Hildegar Medina <medinahildegar1@gmail.com>
2024-10-14 18:23:51 -0300

    Add Docker to project

--- cache/config.js
+++ cache/config.js
@@ -1,5 +1,12 @@
 import redis from 'redis';
 import dotenv from 'dotenv';
+
 dotenv.config();
 
-export const redisClient = redis.createClient();
+const url = `redis://${process.env.REDIS_HOST || 'localhost'}:${process.env.REDIS_PORT || 6379}`;
+export const redisClient = redis.createClient({
+    url: url,
+    socket: {
+        connectTimeout: 10000
+    }
+});
